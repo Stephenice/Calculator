02c41dbfaa5b45d7ad66737a5a6d6265050834c9
Stephenice <stephencode@hotmail.com>
2021-01-12 07:33:04 +0100

    calculator

--- script.js
+++ script.js
@@ -1,43 +1,10 @@
 'use strict';
+// select all element 
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButton = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement =  document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
-const calculator = document.querySelector('.calculator');
-const keys = document.querySelector('.calculator__keys');
-const dispaly = document.querySelector('.calculator__display');
-
-keys.addEventListener('click', e => {
-    if(e.target.matches('button')){
-        const key = e.target;
-        const action = key.dataset.action;
-        const keyContent = key.textContext;
-        const displayedNum = display.textContext;
-
-        if (!action) {
-            if (displayedNum === '0') {
-              display.textContent = keyContent;
-            } else {
-              display.textContent = displayedNum + keyContent;
-            }
-          }
-
-
-          if (action === 'decimal') {
-            display.textContent = displayedNum + '.';
-          }
-
-          if (
-            action === 'add' ||
-            action === 'subtract' ||
-            action === 'multiply' ||
-            action === 'divide'
-          ) {
-            key.classList.add('is-depressed')
-          }
-
-
-
-
-
-
-
-    }
-})
